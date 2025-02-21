571025fa87e16873d6f4831ea11dec46e6fe2a7c
Eoin Treacy <eointreacy5@gmail.com>
2025-02-21 16:24:34 +0000

    .

--- init.lua
+++ init.lua
@@ -1,7 +1,5 @@
 print("Hi Eoin!") 
 
-require("config.lazy")
-
 vim.api.nvim_create_autocmd('TextYankPost', {
 	desc = 'Highlight when yanking text',
 	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
