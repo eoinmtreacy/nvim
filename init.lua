11a4eabe51b58024f1ef0ec1570e81831f4e6e03
Eoin Treacy <eointreacy5@gmail.com>
2025-02-23 13:04:28 +0000

    remove mason for lspconfig

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 print("Hi Eoin!") 
 require("config.lazy")
 
+vim.opt.shiftwidth 	= 4
+vim.opt.number 		= true
+vim.opt.relativenumber 	= true
+vim.opt.clipboard 	= "unnamedplus"
+
 vim.api.nvim_create_autocmd('TextYankPost', {
 	desc = 'Highlight when yanking text',
 	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
