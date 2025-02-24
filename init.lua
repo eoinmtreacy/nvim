378d46549485d1c87bc99c18446cb62a81268c1e
Eoin Treacy <eointreacy5@gmail.com>
2025-02-24 07:59:41 +0000

    mod init and lazy lock

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ vim.opt.shiftwidth 	= 4
 vim.opt.number 		= true
 vim.opt.relativenumber 	= true
 vim.opt.clipboard 	= "unnamedplus"
+vim.opt.termguicolors	= true
 
 vim.api.nvim_create_autocmd('TextYankPost', {
 	desc = 'Highlight when yanking text',
